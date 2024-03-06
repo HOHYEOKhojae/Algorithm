9e01a5b109477acde2c4787787f92fd3063c30ed
kkunh97@naver.com <kkunh97@naver.com>
2024-03-06 22:04:19 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 김건혁 on 3/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
