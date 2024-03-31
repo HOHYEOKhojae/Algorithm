2e5fa091b3de1c78f8ba69e3e0adfe6380fb87b6
kkunh97@naver.com <kkunh97@naver.com>
2024-03-31 12:02:58 +0900

    # Easy problem - Think about greatest common divisor.

---
+++ Algorithm/problem07.cpp
@@ -0,0 +1,8 @@
+//
+//  problem07.cpp
+//  Algorithm
+//
+//  Created by 김건혁 on 3/31/24.
+//
+
+#include "problem07.hpp"
---
+++ Algorithm/problem07.hpp
@@ -0,0 +1,13 @@
+//
+//  problem07.hpp
+//  Algorithm
+//
+//  Created by 김건혁 on 3/31/24.
+//
+
+#ifndef problem07_hpp
+#define problem07_hpp
+
+#include <stdio.h>
+
+#endif /* problem07_hpp */
