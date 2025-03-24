0da5907a157c4ccc6165c72b8fec9318f7ea8580
hyeokinen <87744606+hyeokinen@users.noreply.github.com>
2025-03-24 17:13:24 +0900

    Create pointer_example_0324.cpp
    포인터 설명을 위한 예제

---
+++ Algorithm/pointer_example_0324.cpp
@@ -0,0 +1,19 @@
+#include stdio.h
+#include "finc1"
+
+void func1()
+{
+  int a = 3;
+  int b = 4;
+}
+
+void func1()
+
+int main()
+{
+  int a = 1;
+  int b = 2;
+  func1();
+
+  // a,b 의 값은 어떻게 되는가?
+}
